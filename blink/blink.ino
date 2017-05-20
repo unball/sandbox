d791e871dd6637d2ecf486e216afee8a3fb17495
Anderson Barros Rodrigues <makiander.abr@gmail.com>
2017-05-20 15:50:53 -0300

    Added function move

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,4 +1,27 @@
 int led=21;
+int pwm = 9;
+int stby = 8;
+int ain1 = 7;
+int ain2 = 6;
+
+void move(int valor){
+  pinMode(pwm, OUTPUT);
+  pinMode(stby, OUTPUT);
+  pinMode(ain1, OUTPUT);
+  pinMode(ain2, OUTPUT);
+  
+  if(valor>255){
+    valor = 255;
+  }
+  else if(valor<0){
+    valor = 0;
+  } 
+  
+  analogWrite(pwm, valor);
+  digitalWrite(stby, HIGH);
+  digitalWrite(ain1, HIGH);
+  digitalWrite(ain2, LOW);
+  }
 
 void setup() {
   pinMode(led,OUTPUT);
@@ -7,9 +30,6 @@ void setup() {
 
 void loop() {
   
-  digitalWrite(led,HIGH);
-  delay(5000);
-  digitalWrite(led,LOW);
-  delay(5000);
+  move(200);
 
 }
