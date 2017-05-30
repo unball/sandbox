27ce4ffe214bef0c22045fc43baf6dfae2536479
Igor Beduin <igorbeduin@MacBook-Pro-de-Igor.local>
2017-05-30 16:32:10 -0300

    Code order correction

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,61 +1,42 @@
-int m;
-char incomingByte;
 int pwm = 9;
 int stby = 8;
 int ain1 = 7;
 int ain2 = 6;
 
-
-
-void move(int valor){
+void setup(){
+  Serial.begin(9600); //baudrate
+  
   pinMode(pwm, OUTPUT);
   pinMode(stby, OUTPUT);
   pinMode(ain1, OUTPUT);
   pinMode(ain2, OUTPUT);
+}
+
+int m=0;
+
+void move(int valor){
   
   if(valor>255){
     valor = 255;
   }
+  
   else if(valor<0){
     valor = 0;
   } 
-  
+
   analogWrite(pwm, valor);
   digitalWrite(stby, HIGH);
   digitalWrite(ain1, HIGH);
   digitalWrite(ain2, LOW);
+  
 }
-
-void setup(){
-  Serial.begin(9600); //baudrate
-}
-
 
 void loop() {
   
+  if(Serial.available()>0){
+   m=Serial.parseInt();
+   Serial.println(m);
+  }
 
-  if (Serial.available() > 0){
-    m = 0;
-    while (1){
-      incomingByte = Serial.read ();
-      if (incomingByte == '\n') break;
-      if (incomingByte == -1) continue;
-      m *= 10;
-      m = ((incomingByte - 48) + m);
-    }
-     if (m > 255){
-        m = 255;
-     }
-     if (m < 0){
-      m = 0;
-     }
-      Serial.println(m);   
-   }
-   
-    
-   
-   
-  
   move(m);
-  
 }
